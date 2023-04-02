cbf6572f98bcaef29469c6e0695a61fdcf37f886
박다정 <ekwjd990913@gmail.com>
2023-04-02 12:55:22 +0900

    Issue domain modeling

--- issue-service/src/main/kotlin/fastcampus/issueservice/domain/enums/IssuePriority.kt
+++ issue-service/src/main/kotlin/fastcampus/issueservice/domain/enums/IssuePriority.kt
@@ -1,5 +1,10 @@
 package fastcampus.issueservice.domain.enums
 
 enum class IssuePriority {
+    LOW, MEDIUM, HIGH;
 
+    companion object {
+        //operator를 이용해 함수명을 사용하지 않고 생성자처럼 초기화할 수 있음
+        operator fun invoke(priority: String) = valueOf(priority.uppercase())
+    }
 }
--- issue-service/src/main/kotlin/fastcampus/issueservice/domain/enums/IssueType.kt
+++ issue-service/src/main/kotlin/fastcampus/issueservice/domain/enums/IssueType.kt
@@ -1,4 +1,18 @@
 package fastcampus.issueservice.domain.enums
 
 enum class IssueType {
+    //type 정의
+    BUG, TASK;
+
+    // 외부에서 사용할 수 있게 하기 위함
+    companion object {
+        //정적 팩토리 함수 생성 -> java style
+//        fun of(type: String) = valueOf(type.uppercase())
+//        val type = IssueType.of("BUG")
+
+        //kotlin style
+        operator fun invoke(type: String) = valueOf(type.uppercase())
+        //operator를 이용하면 함수명 없이(생성자를 사용한 것처럼) 객체를 초기화 할 수 있다.
+        // val type = IssueType("BUG")
+    }
 }
